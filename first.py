62f2f89b15f713e9c6ac86a2691a9746b4d2fc4b
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-02-02 11:02:06 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,25 @@
+# python first.py
+print("Hello World")
+
+Salary = True
+
+def ChakTrue(ChakTrue):
+	if ChakTrue == True:
+		print("The valu is true")
+	else :
+		print("the valu is fals")
+
+ChakTrue(Salary)
+# --------
+pri = True,"hello"
+print(pri)
+
+# ------
+for i in range(5):
+	print(hex(120))
+
+for i in range(7):
+	print("gfdslfsdsfd;".encode().hex())
+
+
+
---
+++ modiol.py
@@ -0,0 +1,22 @@
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+
+# this is comant
+
+""" 
+this is comant 
+fdsjk;fjpasjfdsl
+"""
+"""fsdfafdfskjfldjkd"""
+
+
+# sdfkl;jas;lfdsjka
+# sdkjfkl;jas
+# fdkjsal;fdkjsal
+# fas;f
+
+
+
