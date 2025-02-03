e80d401a86ba3026c83e02ed5282100644408757
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-02-03 12:55:28 +0600

    update string.py

---
+++ string.py
@@ -0,0 +1,23 @@
+name = "ashraful"
+
+print(name)
+# fint a letter
+print(name[5])
+
+# cut the name
+print(name[5:])
+print(name[5:8])
+print(name[5:7])
+print(name[0:])
+
+# --------
+
+poim = """ 
+ha ha ha ha ha 
+hahahaha ha 
+ 	"""
+print(poim)
+
+poit = " this is a poit and \n ths is my leter"
+print(poit)
+
