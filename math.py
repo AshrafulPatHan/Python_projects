105dba9bebbb967519bf09a983bc144af16b7df4
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-02-03 12:18:15 +0600

    update math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+a = 15
+b = 28
+c = -24
+d = 2.556
+c = -4.54
+
+# -----
+print(100%10)
+# --
+print(28%5)
+# ---
+print(4^2) # this is the worong mathod for mack a squer
+
+print(4**2) # thsi is the rite method for mack a squer
+
