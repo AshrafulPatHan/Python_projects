872bdcf1a11325a769369e43c63a2dec0c522353
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-11 15:52:45 +0600

    registratin is add sucesfully

--- project/LMS/routes/auth/routes.py
+++ project/LMS/routes/auth/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import request,Blueprint,jsonify
+from DB import mysql, init_app
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -6,6 +7,19 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 def login():
     return "Login Page"
 
-@auth_bp.route('/register')
+@auth_bp.route('/register',methods=['POST'])
 def register():
-    return "Register Page"
+    data = request.get_json()
+    name = data['username']
+    email = data['email']
+    password = data['password']
+    role = 'user'
+    cursor = mysql.connection.cursor()
+    cursor.execute(
+        "INSERT INTO Users (username, email, password, role) VALUES (%s, %s, %s, %s)",
+        (name, email, password, role)
+    )
+    mysql.connection.commit()
+    cursor.close()
+    result= jsonify({"message":"User is add successfully"}), 201
+    return result
