2a0ab912a43ca86fedefc05967d396ea99ed6efc
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-11 12:13:04 +0600

    add all html page in to / rouses page

--- project/LMS/main.py
+++ project/LMS/main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from routes.auth.routes import auth_bp
 from routes.admin.routes import admin_bp
 from routes.public.routes import public_bp
+from routes.page.routes import page_bp
 import os
 from DB import mysql, init_app
 
@@ -45,6 +46,7 @@ def HTML_SERVER():
 app.register_blueprint(auth_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(public_bp)
+app.register_blueprint(page_bp)
 
 
 if __name__ == '__main__':
---
+++ project/LMS/routes/page/routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint,render_template
+
+page_bp = Blueprint('page',__name__,url_prefix='/page')
+
+# 📜 Login
+@page_bp.route('/login')
+def Login_page():
+    return render_template("./page/login.html")
+
+# 📜 register
+@page_bp.route('/register')
+def register_page():
+    return render_template("./page/register.html")
+
+# 📜 Book
+@page_bp.route('/book')
+def book_page():
+    return render_template("./page/book_list.html")
+
+# 📜 Admin
+@page_bp.route('/admin')
+def admin_page():
+    return render_template("./page/admin_dashboard.html")
+
