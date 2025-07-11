8268627d6a0e613b4def92b025fa74f46917c482
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-11 16:07:06 +0600

    login is add

--- project/LMS/routes/auth/routes.py
+++ project/LMS/routes/auth/routes.py
@@ -1,18 +1,53 @@
 from flask import request,Blueprint,jsonify
 from DB import mysql, init_app
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['POST'])
 def login():
-    return "Login Page"
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM Users WHERE email = %s", (email,))
+    result = cursor.fetchone()
+
+    # data is coming for database:
+    user = {
+        "id": result[0],
+        "username": result[1],
+        "email": result[2],
+        "password": result[3],
+        "role": result[4]
+    }
+    cursor.close()
+
+    if user and check_password_hash(user['password'], password):
+        # You can generate a session or JWT token here if needed
+        return jsonify({
+            "message": "Login successful",
+            "user": {
+                "id": user['id'],
+                "username": user['username'],
+                "email": user['email'],
+                "role": user['role']
+            }
+        }), 200
+    else:
+        return jsonify({"message": "Invalid email or password"}), 401
+
 
 @auth_bp.route('/register',methods=['POST'])
 def register():
     data = request.get_json()
     name = data['username']
     email = data['email']
-    password = data['password']
+    # password = data['password']
+    # hashed password registration
+    password = generate_password_hash(data['password'])
     role = 'user'
     cursor = mysql.connection.cursor()
     cursor.execute(
